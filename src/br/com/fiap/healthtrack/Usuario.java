6f57a6fa72fdd7b7e3c18c427fd5ea3a82ff55f9
Aline Soares <alinedmsoares@gmail.com>
2021-08-25 20:58:22 -0300

    up

---
+++ src/br/com/fiap/healthtrack/Usuario.java
@@ -0,0 +1,65 @@
+package br.com.fiap.healthtrack;
+
+import java.util.Date;
+
+public class Usuario {
+	
+	private int idUsuario;
+	private String nome;
+	private Date dtNascimento;
+	private String genero;
+	private Double altura;
+	private String email;
+	private String senha;
+	
+	public int getIdUsuario() {
+		return idUsuario;
+	}
+	public void setIdUsuario(int idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public Date getDtNascimento() {
+		return dtNascimento;
+	}
+	public void setDtNascimento(Date dtNascimento) {
+		this.dtNascimento = dtNascimento;
+	}
+	
+	public String getGenero() {
+		return genero;
+	}
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+	
+	public Double getAltura() {
+		return altura;
+	}
+	public void setAltura(Double altura) {
+		this.altura = altura;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}	
+	
+	
+
+}
